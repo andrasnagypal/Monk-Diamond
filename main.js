108f6db7b42b928e7ffd6bf929e989dee5c2b80c
unknown <nagandsoft@gmail.com>
2019-01-30 19:38:11 +0100

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var cycles=0;
+var visibleCharacters=false;
+var score=0;
+
 function passwordCheck()
 		{
 			var password=document.getElementById("passwordBox");
@@ -31,4 +35,39 @@ function loadList()
 	document.getElementById("list").innerHTML=localStorage.storedList;
 	for (var i=0;i<list.children.length;i++)
 		list.children[i].onclick=deleteItem;
+}
+function startGame()
+{
+	gamePlayLoop();
+}
+function gamePlayLoop()
+{
+	visibleCharacters=!visibleCharacters;
+	characterCreation();	
+	cycles++;
+	alert(cycles);
+	if (cycles<12)
+	setTimeout(gamePlayLoop,visibleCharacters ? 1000 :3000);
+else alert("Score: "+ score);
+}
+
+function characterCreation()
+{
+	var tablet=document.getElementById("board");
+	var classSetter=visibleCharacters ? "character visible":"character hidden";
+	for (var index=0;index<8;index++)
+	{
+		tablet.children[index].className=classSetter;
+		tablet.children[index].innerHTML="";
+		tablet.children[index].onclick=function(){
+			score-=2;
+		}
+	}
+	var rnd=Math.floor(Math.random()*7)+1;
+	console.log(rnd);
+	tablet.children[rnd].innerHTML="";
+	tablet.children[rnd].onclick=function(){
+			score+=1;
+		}
+		tablet.children[rnd].className=classSetter+ " thief";
 }
